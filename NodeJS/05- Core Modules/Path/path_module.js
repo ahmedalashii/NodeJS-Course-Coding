c075d9daf3e2c7fbd3676fe3825bac729d41ca5b
ahmed_alashii <you@example.com>
2023-04-18 19:12:09 +0200

    Changes ..

--- NodeJS/05- Core Modules/Path/path_module.js
+++ NodeJS/05- Core Modules/Path/path_module.js
@@ -1,16 +1,29 @@
 const path = require('path');
 
 
-console.log(__filename);
+console.log("File name: ", __filename);
 // Base file name (path_module.js) using path.basename()
-console.log(path.basename(__filename));
+console.log("Base", path.basename(__filename));
 // Directory name (Path) using path.dirname()
-console.log(path.dirname(__filename));
+console.log("Directory", path.dirname(__filename));
 // File extension (.js) using path.extname()
-console.log(path.extname(__filename));
+console.log("Extension", path.extname(__filename));
 // Create path object using path.parse()
-console.log(path.parse(__filename));
+console.log("Filename", path.parse(__filename));
 // Concatenate paths using path.join()
-console.log(path.join(__dirname, 'test', 'hello.html'));
+console.log(path.join('../data', 'story.text'));
+// Concatenate paths using path.join()
+console.log("Concatenate", path.join(__dirname, '../data', 'story.text'));
 // Get the absolute path using path.resolve()
-console.log(path.resolve(__dirname, 'test', 'hello.html'));
+console.log("Absolute", path.resolve(__dirname, '../data', 'story.text'));
+
+const { readFile } = require('fs');
+readFile(path.join(__dirname, '../data', 'story.text'), 'utf8', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
+
+// path.join is useful when you want to concatenate paths regardless of the OS you are using.
---
+++ NodeJS/06- NPM Modules/Project 1/app.js
@@ -0,0 +1,2 @@
+const express = require('express'); // First it will be searched in the core modules, if found then it's good, if it;'s not found then it will be searched in npm_modules
+
